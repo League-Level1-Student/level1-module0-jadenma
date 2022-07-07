85faf7648463e930408f63ea1be997ecee95f4fb
root <root@033fe3c947eb>
2022-07-07 02:56:42 +0000

    Finished Body Part Quiz, and did duelling buttons.

--- src/_03_gui_with_help/_4_body_part_quiz/BodyPartQuiz.java
+++ src/_03_gui_with_help/_4_body_part_quiz/BodyPartQuiz.java
@@ -1,5 +1,7 @@
 package _03_gui_with_help._4_body_part_quiz;
 
+import java.net.URL;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -21,10 +23,10 @@ public class BodyPartQuiz {
 	// package,or if you prefer, get celebrity photos from the Internet,
 	// place them in the recipe package(body_part_quiz), and change the names below.
 
-	String firstImage = "src/_05_body_part_quiz/arnold.jpeg";
-	String secondImage = "src/_05_body_part_quiz/leonardo.jpeg";
-	String thirdImage = "src/_05_body_part_quiz/morgan.jpeg";
-	String fourthImage = "src/_05_body_part_quiz/jack.jpeg";
+	String firstImage = "arnold.jpeg";
+	String secondImage = "leonardo.jpeg";
+	String thirdImage = "morgan.jpeg";
+	String fourthImage = "jack.jpeg";
 
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
@@ -38,11 +40,12 @@ public class BodyPartQuiz {
 		initializeImageList();
 		imageIterator = imageList.iterator();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//panel.add(imageIterator);
 		window.add(panel);
 		
 		// 3. Change the size of the window so that you can only see part of the
 		// image.
-		window.setSize(500, 500);
+		window.setSize(500, 150);
 
 		showNextImage();
 
@@ -55,11 +58,17 @@ public class BodyPartQuiz {
 		// 2. Set the size of the window in the initializeGui() method 
 
 		// 4. Ask the user who this person is and store their answer
-		String guess = JOptionPane.showInputDialog("who is this?");
+		String guess = JOptionPane.showInputDialog("Who is this?");
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-		
+		if (guess.equals("Arnold Schwarzenegger")) {
+			JOptionPane.showMessageDialog(null, "You are correct!");
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "This is Arnold Schwarzenegger");
+		}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
 
@@ -67,9 +76,34 @@ public class BodyPartQuiz {
 		showNextImage();
 
 		// 8. .... repeat 4-7 for all your images.....
-
+		String guess2 = JOptionPane.showInputDialog("Who is this?");
+		if (guess2.equals("Leonardo DiCaprio")) {
+			JOptionPane.showMessageDialog(null, "You are correct!");
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "This is Leonardo DiCaprio.");
+		}
+		showNextImage();
+		String guess3 = JOptionPane.showInputDialog("Who is this?");
+		if (guess3.equals("Morgan Freeman")) {
+			JOptionPane.showMessageDialog(null, "You are correct!");
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "This is Morgan Freeman.");
+		}
+		showNextImage();
+		String guess4 = JOptionPane.showInputDialog("Who is this?");
+		if (guess4.equals("Jack Black")) {
+			JOptionPane.showMessageDialog(null, "You are correct!");
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "This is Jack Black.");
+		}
 		// 9. Show them their current score
-
+		JOptionPane.showMessageDialog(null, "Your final score is " + score);
 	}
 
 	public void showNextImage() {
@@ -79,8 +113,11 @@ public class BodyPartQuiz {
 	}
 
 	private JLabel loadImage(String fileName) {
-		Icon icon = new ImageIcon(fileName);
-		JLabel label = new JLabel(icon);
+		
+		JLabel label = new JLabel();
+		URL imgUrl = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imgUrl);
+		label.setIcon(icon);
 		return label;
 	}
 
@@ -89,6 +126,7 @@ public class BodyPartQuiz {
 
 	private void initializeImageList() {
 		JLabel imageLabel = loadImage(firstImage);
+		
 		imageList.add(imageLabel);
 		imageLabel = loadImage(secondImage);
 		imageList.add(imageLabel);
